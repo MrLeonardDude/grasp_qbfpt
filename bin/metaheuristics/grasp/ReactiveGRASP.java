15bcccc906b82c048be37ea1e93618d48bfa34f2
CintiaMuranaka <49293845+CintiaMuranaka@users.noreply.github.com>
2019-04-08 00:15:51 -0300

    Método Reativo

---
+++ bin/metaheuristics/grasp/ReactiveGRASP.java
@@ -0,0 +1,286 @@
+/**
+ * MO824A - Atividade 3
+ * Alunos: 262212, 228113, 172017
+ *  */
+package metaheuristics.grasp;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import problems.Evaluator;
+import solutions.Solution;
+
+/**
+ * Abstract class for metaheuristic GRASP (Greedy Randomized Adaptive Search
+ * Procedure). It consider a minimization problem.
+ * 
+ * @author ccavellucci, fusberti
+ * @param <E>
+ *            Generic type of the element which composes the solution.
+ */
+public abstract class ReactiveGRASP<E> {
+
+	/**
+	 * flag that indicates whether the code should print more information on
+	 * screen
+	 */
+	public static boolean verbose = true;
+
+	/**
+	 * a random number generator
+	 */
+	static Random rng = new Random(0);
+
+	/**
+	 * the objective function being optimized
+	 */
+	protected Evaluator<E> ObjFunction;
+
+	/**
+	 * the GRASP greediness-randomness parameter
+	 */
+	protected Double alpha;
+	protected Double alphaList[] = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07};
+
+	/**
+	 * the best solution cost
+	 */
+	protected Double bestCost;
+
+	/**
+	 * the incumbent solution cost
+	 */
+	protected Double incumbentCost;
+
+	/**
+	 * the best solution
+	 */
+	protected Solution<E> bestSol;
+
+	/**
+	 * the incumbent solution
+	 */
+	protected Solution<E> incumbentSol;
+
+	/**
+	 * the number of iterations the GRASP main loop executes.
+	 */
+	protected Integer iterations;
+
+	/**
+	 * the Candidate List of elements to enter the solution.
+	 */
+	protected ArrayList<E> CL;
+
+	/**
+	 * the Restricted Candidate List of elements to enter the solution.
+	 */
+	protected ArrayList<E> RCL;
+
+	private double[] ps;
+
+	private double[] q;
+
+	/**
+	 * Creates the Candidate List, which is an ArrayList of candidate elements
+	 * that can enter a solution.
+	 * 
+	 * @return The Candidate List.
+	 */
+	public abstract ArrayList<E> makeCL();
+
+	/**
+	 * Creates the Restricted Candidate List, which is an ArrayList of the best
+	 * candidate elements that can enter a solution. The best candidates are
+	 * defined through a quality threshold, delimited by the GRASP
+	 * {@link #alpha} greedyness-randomness parameter.
+	 * 
+	 * @return The Restricted Candidate List.
+	 */
+	public abstract ArrayList<E> makeRCL();
+
+	/**
+	 * Updates the Candidate List according to the incumbent solution
+	 * {@link #incumbentSol}. In other words, this method is responsible for
+	 * updating which elements are still viable to take part into the solution.
+	 */
+	public abstract void updateCL();
+
+	/**
+	 * Creates a new solution which is empty, i.e., does not contain any
+	 * element.
+	 * 
+	 * @return An empty solution.
+	 */
+	public abstract Solution<E> createEmptySol();
+
+	/**
+	 * The GRASP local search phase is responsible for repeatedly applying a
+	 * neighborhood operation while the solution is getting improved, i.e.,
+	 * until a local optimum is attained.
+	 * 
+	 * @return An local optimum solution.
+	 */
+	public abstract Solution<E> localSearch();
+
+	/**
+	 * Constructor for the AbstractGRASP class.
+	 * 
+	 * @param objFunction
+	 *            The objective function being minimized.
+	 * @param alpha
+	 *            The GRASP greediness-randomness parameter (within the range
+	 *            [0,1])
+	 * @param iterations
+	 *            The number of iterations which the GRASP will be executed.
+	 */
+	public ReactiveGRASP(Evaluator<E> objFunction, Integer iterations) {
+		this.ObjFunction = objFunction;
+		this.iterations = iterations;
+	}
+	
+	/**
+	 * The GRASP constructive heuristic, which is responsible for building a
+	 * feasible solution by selecting in a greedy-random fashion, candidate
+	 * elements to enter the solution.
+	 * 
+	 * @return A feasible solution to the problem being minimized.
+	 */
+	public Solution<E> constructiveHeuristic() {
+
+		CL = makeCL();
+		RCL = makeRCL();
+		incumbentSol = createEmptySol();
+		incumbentCost = Double.POSITIVE_INFINITY;
+
+		/* Main loop, which repeats until the stopping criteria is reached. */
+		while (!constructiveStopCriteria()) {
+
+			double maxCost = Double.NEGATIVE_INFINITY, minCost = Double.POSITIVE_INFINITY;
+			incumbentCost = ObjFunction.evaluate(incumbentSol);
+			updateCL();
+
+			/*
+			 * Explore all candidate elements to enter the solution, saving the
+			 * highest and lowest cost variation achieved by the candidates.
+			 */
+			for (E c : CL) {
+				Double deltaCost = ObjFunction.evaluateInsertionCost(c, incumbentSol);
+				if (deltaCost < minCost)
+					minCost = deltaCost;
+				if (deltaCost > maxCost)
+					maxCost = deltaCost;
+			}
+
+			/*
+			 * Among all candidates, insert into the RCL those with the highest
+			 * performance using parameter alpha as threshold.
+			 */
+			
+			for (E c : CL) {
+				Double deltaCost = ObjFunction.evaluateInsertionCost(c, incumbentSol);
+				if (deltaCost <= minCost + alpha * (maxCost - minCost)) {
+					RCL.add(c);
+				}
+			}
+
+			/* Choose a candidate randomly from the RCL */
+			int rndIndex = rng.nextInt(RCL.size());
+			E inCand = RCL.get(rndIndex);
+			CL.remove(inCand);
+			incumbentSol.add(inCand);
+			ObjFunction.evaluate(incumbentSol);
+			RCL.clear();
+
+		}
+
+		return incumbentSol;
+	}
+
+	/**
+	 * The GRASP mainframe. It consists of a loop, in which each iteration goes
+	 * through the constructive heuristic and local search. The best solution is
+	 * returned as result.
+	 * 
+	 * @return The best feasible solution obtained throughout all iterations.
+	 */
+	public Solution<E> solve() {
+
+		bestSol = createEmptySol();
+			
+		int V = alphaList.length;
+		ps = new double[V];
+		q = new double[V];
+				
+		for (int k=0; k < V; k++) {
+			ps[k]= (double) 1/(alphaList.length);
+		}
+		
+		int amplificacao = 10;
+		int auxr = 0;
+		
+		for (int i = 0; i < iterations; i++) {
+			
+			// randomico com peso - de acordo com o percentual de cada peso
+			Random r = new Random();
+			int sorteios = 20;
+			for (int rp = 0; rp < sorteios; rp++) {
+			    double total = 0;
+				double chanceSorteada = r.nextDouble(); // numero entre 0 e 1
+			    for (int j = 0; j < alphaList.length; j++) {
+			        total += ps[j];
+			        if (chanceSorteada <= total) {
+			            alpha = alphaList[j];
+			            break;
+			        }
+			    }
+			}
+	
+			constructiveHeuristic();
+			localSearch();
+			if (bestSol.cost > incumbentSol.cost) {
+				
+				bestSol = new Solution<E>(incumbentSol);
+				if (verbose) {
+					System.out.println("(Iter. " + i + ") BestSol = " + bestSol);
+					}
+				
+			}
+			
+			/*
+			 * Os valores de pk serão atualizados, a cada γ iterações (parametro amplificacao),
+               
+			 */
+			double soma = 0.0;
+			
+			if (i % amplificacao == 0) {
+				double somaPS = 0.0;
+				for (int f=0; f < V; f++) {
+					
+					q[f] = bestSol.cost/alphaList[f];
+					for (int j=0; j < V; j++) {
+						soma += soma + q[j];
+					}
+					ps[f] = q[f]/soma; 
+								
+				}
+									
+			}
+
+		}
+
+		return bestSol;
+	}
+
+	/**
+	 * A standard stopping criteria for the constructive heuristic is to repeat
+	 * until the incumbent solution improves by inserting a new candidate
+	 * element.
+	 * 
+	 * @return true if the criteria is met.
+	 */
+	public Boolean constructiveStopCriteria() {
+		return (incumbentCost > incumbentSol.cost) ? false : true;
+	}
+
+}
